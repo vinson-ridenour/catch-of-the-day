fde65b71816d0b569a86448539eac1be6556092a
Vinson Ridenour <vinson.ridenour@gmail.com>
2020-01-13 17:53:48 -0800

    adds dummy data with button, also updates state

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,41 @@ import React from 'react'
 import Header from './Header'
 import Order from './Order'
 import Inventory from './Inventory'
+import sampleFishes from '../sample-fishes'
 
 class App extends React.Component {
+  // the state and the methods that update state need to live in the exact same component
+  state = {
+    fishes: {},
+    order: {}
+  }
+  addFish = fish => {
+    // take copy of existing state (so you don't "mutate" the state directly)
+    const fishes = { ...this.state.fishes }
+    // add new fish to our fishes variable, with timestamp to make unique
+    fishes[`fish${Date.now()}`] = fish
+    // set the new fishes object to state, if diff detected, will update new state automatically
+    this.setState({
+      fishes
+    })
+  }
+  loadSampleFishes = () => {
+    this.setState({
+      fishes: sampleFishes
+    })
+  }
   render() {
     return (
       <div className='catch-of-the-day'>
         <div className='menu'>
           <Header tagline='Fresh Seafood Market' />
+          <ul className='fishes'></ul>
         </div>
         <Order />
-        <Inventory />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     )
   }
