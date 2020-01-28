19e2c552b721f792d4d90a6872b3e6184be9f55d
Vinson Ridenour <vinson.ridenour@gmail.com>
2020-01-28 15:26:16 -0800

    adds deployment to netlify with _redirects file after any modifications made

---
+++ build/precache-manifest.560de2c9feca1dffba2ae22c2ff0162f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea81ec7c35fa60cae30e44b4893e882",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfac410e7519462736a3",
+    "url": "/static/css/main.36035509.chunk.css"
+  },
+  {
+    "revision": "4a06286a2de4f0fd0249",
+    "url": "/static/js/2.51284fd5.chunk.js"
+  },
+  {
+    "revision": "a8c6aca52063a857962cec4d6f5ca45d",
+    "url": "/static/js/2.51284fd5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bfac410e7519462736a3",
+    "url": "/static/js/main.fa9ec644.chunk.js"
+  },
+  {
+    "revision": "794cb3a239c16f167a95",
+    "url": "/static/js/runtime-main.080bbe98.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  }
+]);
